e9048dc6b68e579aa3e8b296486ed3b5556c4a1d
TheDrianN <edelgadovidarte@gmail.com>
2024-11-08 16:05:14 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    apiBaseUrl: 'https://k9njpwzang.us-east-2.awsapprunner.com', // URL base para tu API
+    apiBaseUrl: 'https://nphks3wv56.us-east-2.awsapprunner.com', // URL base para tu API
   };
 
   export default config;
