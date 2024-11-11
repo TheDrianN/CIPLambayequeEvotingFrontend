31f50fc3ecbe0d7d66fd408dc4cc686a8e902eb8
TheDrianN <edelgadovidarte@gmail.com>
2024-11-11 12:35:01 -0500

    la ultima esperanza

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const config = {
-    apiBaseUrl: 'https://nphks3wv56.us-east-2.awsapprunner.com', // URL base para tu API
+    apiBaseUrl: 'https://zpn77qpifg.us-east-2.awsapprunner.com', // URL base para tu API
   };
 
   export default config;
